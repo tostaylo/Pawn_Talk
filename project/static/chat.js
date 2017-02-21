9c7f746fa7ce4e13f9d5abc042c3afa27d9f700d
tostaylo <tostaylo1@gmail.com>
2017-02-21 13:54:33 -0800

    started styling of chat page

--- project/static/chat.js
+++ project/static/chat.js
@@ -1,11 +1,16 @@
-
 //SOCKET CONNECTION
- let socket = io.connect('//' + document.domain + ':' + location.port);
+let socket = io.connect('//' + document.domain + ':' + location.port);
 
 var localVideo;
 var remoteVideo;
 var peerConnection;
-var peerConnectionConfig = {'iceServers': [{'url': 'stun:stun.services.mozilla.com'}, {'url': 'stun:stun.l.google.com:19302'}]};
+var peerConnectionConfig = {
+    'iceServers': [{
+        'url': 'stun:stun.services.mozilla.com'
+    }, {
+        'url': 'stun:stun.l.google.com:19302'
+    }]
+};
 
 navigator.getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
 window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
@@ -25,7 +30,7 @@ function pageReady() {
         audio: true,
     };
 
-    if(navigator.getUserMedia) {
+    if (navigator.getUserMedia) {
         navigator.getUserMedia(constraints, getUserMediaSuccess, getUserMediaError);
     } else {
         alert('Your browser does not support getUserMedia API');
@@ -50,7 +55,7 @@ function start(isCaller) {
     peerConnection.onaddstream = gotRemoteStream;
     peerConnection.addStream(localStream);
 
-    if(isCaller) {
+    if (isCaller) {
         peerConnection.createOffer(gotDescription, createOfferError);
     }
 }
@@ -59,13 +64,19 @@ function start(isCaller) {
 function gotDescription(description) {
     console.log('got description');
     peerConnection.setLocalDescription(description, function () {
-        socket.emit('video', JSON.stringify({'sdp': description}));
-    }, function() {console.log('set description error')});
+        socket.emit('video', JSON.stringify({
+            'sdp': description
+        }));
+    }, function () {
+        console.log('set description error')
+    });
 }
 
 function gotIceCandidate(event) {
-    if(event.candidate != null) {
-        socket.emit('video', JSON.stringify({'ice': event.candidate}));
+    if (event.candidate != null) {
+        socket.emit('video', JSON.stringify({
+            'ice': event.candidate
+        }));
     }
 }
 
@@ -80,16 +91,16 @@ function createOfferError(error) {
 
 //ANSWERING THE CliENt
 function gotMessageFromServer(message) {
-    if(!peerConnection) start(false);
+    if (!peerConnection) start(false);
 
     var signal = JSON.parse(message.data);
-    if(signal.sdp) {
-        peerConnection.setRemoteDescription(new RTCSessionDescription(signal.sdp), function() {
-            if(signal.sdp.type == 'offer') {
+    if (signal.sdp) {
+        peerConnection.setRemoteDescription(new RTCSessionDescription(signal.sdp), function () {
+            if (signal.sdp.type == 'offer') {
                 peerConnection.createAnswer(gotDescription, createAnswerError);
             }
         });
-    } else if(signal.ice) {
+    } else if (signal.ice) {
         peerConnection.addIceCandidate(new RTCIceCandidate(signal.ice));
     }
 }
@@ -102,77 +113,82 @@ function gotMessageFromServer(message) {
 
 
 
+/* socket.on('connect', function() {
+     socket.send('user connected');
+ });
 
-
-   /* socket.on('connect', function() {
-        socket.send('user connected');
-    });
-
-    socket.on('disconnect', function() {
-        socket.send('user disconnected')
-    });*/
+ socket.on('disconnect', function() {
+     socket.send('user disconnected')
+ });*/
 
 
 
-    socket.on('message', function(msg){
+socket.on('message', function (msg) {
     let username = $('#username_input').val();
     console.log(msg.username);
     console.log(username);
-    if(msg.username === username){
+    if (msg.username === username) {
         console.log('username is what you entered')
-        $('#messages').append('<li class="right"><span class ="span_right">' + msg.data +  '</span></li><li class="buffer">li</li>');
+        $('#messages').append('<li class="right"><span class ="span_right">' + msg.data + '</span></li><li class="buffer">li</li>');
         $("#messages").scrollTop($('#messages').height())
     } else {
-        $('#messages').append('<li class="left"><span class ="span_left">' + msg.data +  '</span></li><li class="buffer">li</li>');
+        $('#messages').append('<li class="left"><span class ="span_left">' + msg.data + '</span></li><li class="buffer">li</li>');
         $("#messages").scrollTop($('#messages').height())
-        }
-    });
+    }
+});
 
-    socket.on('username_message', function(msg){
+socket.on('username_message', function (msg) {
     console.log('message received');
+});
+
+
+$('#send_button').on('click', function () {
+    socket.emit('message', {
+        'data': $('#my_message').val()
     });
+    $('#my_message').val("")
+    console.log($('#my_message').val());
+    return false;
+});
 
-
-    $('#send_button').on('click', function(){
-        socket.emit('message', {'data': $('#my_message').val()});
-        $('#my_message').val("")
-        console.log($('#my_message').val());
-        return false;
+$('#send_username_button').on('click', function () {
+    socket.emit('username_message', {
+        'data': $('#username_input').val()
     });
-
-     $('#send_username_button').on('click', function(){
-        socket.emit('username_message', {'data': $('#username_input').val()});
-        console.log($('#username_input').val())
-        return false;
-    });
+    console.log($('#username_input').val())
+    return false;
+});
 
 
 
 
 
- $('#link').text(window.location.href);
+
 
 //COPY URL TO CLIPBOARD
+$('#link').text(window.location.href);
 var copyBtn = document.querySelector('#copy');
 
-    copyBtn.addEventListener('click', function () {
+copyBtn.addEventListener('click', function () {
     var link = document.querySelector('#link');
 
-      // create a Range object
-      var range = document.createRange();
-      // set the Node to select the "range"
-      range.selectNode(link);
-      // add the Range to the set of window selections
-      window.getSelection().addRange(range);
+    // create a Range object
+    var range = document.createRange();
+    // set the Node to select the "range"
+    range.selectNode(link);
+    // add the Range to the set of window selections
+    window.getSelection().addRange(range);
 
-      // execute 'copy', can't 'cut' in this case
-      document.execCommand('copy');
-    }, false);
-
-
+    // execute 'copy', can't 'cut' in this case
+    document.execCommand('copy');
+}, false);
 
 
 
 
+//Hide Username Overlay On click
+$('#send_username_button').on('click', function () {
+    $('#username_overlay').css('visibility', 'hidden');
+});
 
-
+document.getElementById("username_input").focus();
